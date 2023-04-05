4ac86e88e1b0f54b6ce8d8d65923b9d0da8b22fa
Mark Conroy <mark@annertech.com>
2023-04-05 20:38:42 +0200

    fixes timers running too fast, stops timer on stop button click

--- web/script.js
+++ web/script.js
@@ -1,24 +1,38 @@
 const addNewButton = document.querySelector(".global-data__button--add-new");
 const tableBody = document.querySelector(".timer-table__landmark--body");
 const tableTemplate = document.querySelector(".timer-table__template");
+let startTimer;
 let currentTimerTime;
 let currentTimerTimeSlot;
 let totalTime;
 
+function convertSecondsToTime(totalSeconds) {
+
+  const totalMinutes = Math.floor(totalSeconds / 60);
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  const seconds = totalSeconds % 60;
+
+  return { h: hours, m: minutes, s: seconds };
+}
+
 function startTheClock() {
   const activeTimerStartButton = document.querySelector('[data-running="true"]');
   const activeTimer = activeTimerStartButton.closest('.timer-table__row');
+
   currentTimerTimeSlot = activeTimer.querySelector('.timer-table__cell--time-spent');
   currentTimerTime = parseInt(activeTimer.querySelector('.timer-table__cell--time-spent').innerText);
-  console.log(currentTimerTime);
-  setInterval(() => {
+
+  clearInterval(startTimer);
+  startTimer = setInterval(() => {
     currentTimerTime = currentTimerTime + 1;
-    currentTimerTimeSlot.innerText = currentTimerTime;
+    const secondsToTime = convertSecondsToTime(currentTimerTime);
+    currentTimerTimeSlot.innerText = `${currentTimerTime >= 3600 ? secondsToTime.h + 'h -' : ''} ${currentTimerTime >= 60 ? secondsToTime.m + 'm -' : ''} ${' ' + secondsToTime.s}s`;
   }, 1000);
 }
 
 function stopTheClock() {
-  console.log('clock stopped');
+  clearInterval(startTimer);
 }
 
 function handleStartButton(e) {
